dd3a36d54c2584e9ef5863bd036ceb9ffa5f7682
benyipCoding <119169478+benyipCoding@users.noreply.github.com>
2023-10-31 11:27:18 +0800

    Delete src/pages/Test directory

--- src/pages/Test/Test.tsx
+++
@@ -1,9 +0,0 @@
-const Test = () => {
-  return (
-    <div className="w-full h-full ">
-      <iframe src="www.example.com" className="w-full h-full"></iframe>
-    </div>
-  );
-};
-
-export default Test;
