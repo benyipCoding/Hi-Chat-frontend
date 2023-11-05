569d089e697d78d66d9e64397d0084dcbc50f7d6
Ben Yip <ben_yip@126.com>
2023-11-05 15:54:34 +0800

    do some package up

---
+++ src/components/Avatar/AvatarDesc.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface AvatarDescProps {
+  userName: string;
+  lastMessage: string;
+}
+
+const AvatarDesc: React.FC<AvatarDescProps> = ({ userName, lastMessage }) => {
+  return (
+    <div className="flex-1 rounded-sm flex flex-col border-b-[1px] border-[#98d3df80]">
+      <p className="flex-1 text-xl flex items-center pl-1 sm:text-2xl">
+        {userName}
+      </p>
+      <p className="flex-1 text-sm flex items-center text-[#b5bac1] pl-1 sm:text-lg">
+        {lastMessage}
+      </p>
+    </div>
+  );
+};
+
+export default AvatarDesc;
--- src/pages/MessagePage/MessagePage.tsx
+++ src/pages/MessagePage/MessagePage.tsx
@@ -1,10 +1,13 @@
 import Avatar from '@/components/Avatar/Avatar';
+import AvatarDesc from '@/components/Avatar/AvatarDesc';
 
 const MessagePage = () => {
+  // Mock Data
   const arr = [1, 2, 3, 4, 5, 6].map((item) => ({
     src: `/images/avatar/${item}.jpeg`,
     userName: `haha${item}`,
   }));
+
   return (
     <div className="lg:flex-1 p-2 flex flex-col gap-2">
       {arr.map((item, index) => (
@@ -13,14 +16,7 @@ const MessagePage = () => {
           key={index}
         >
           <Avatar src={item.src} userName={item.userName} />
-          <div className="flex-1 rounded-sm flex flex-col border-b-[1px] border-[#98d3df80]">
-            <p className="flex-1 text-xl flex items-center pl-1 sm:text-2xl">
-              {item.userName}
-            </p>
-            <p className="flex-1 text-sm flex items-center text-[#b5bac1] pl-1 sm:text-lg">
-              1231231231231
-            </p>
-          </div>
+          <AvatarDesc userName={item.userName} lastMessage="123456" />
         </section>
       ))}
     </div>
